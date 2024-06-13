35710e9770fb6491d63bf6612f28a65ea0d4dd02
iamangus <turnbull.angus@gmail.com>
2024-06-13 18:17:55 -0500

    test

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ ! -d "/data/fooocus/models" ]; then
+    echo "Installing..."
+    git clone https://github.com/lllyasviel/Fooocus.git /data/fooocus
+    cd /data/fooocus
+    #cp settings-no-refiner.json settings.json
+    git reset --hard
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install packaging==23.1 pygit2==1.12.2
+fi
+cd /data/fooocus
+source .venv/bin/activate
+echo "Launching..."
+python entry_with_update.py --listen 0.0.0.0 --port 8080
